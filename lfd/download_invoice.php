a87a433afab9a9e24c750379560241a136e15c59
Vinoth Kumar <vinoth.kumar@zomato.com>
2016-03-16 16:10:06 +0530

    file download vulnerability done

---
+++ lfd/download_invoice.php
@@ -0,0 +1,16 @@
+<?php
+$invoice = dirname(__FILE__).'/invoices/'.$_REQUEST['invoice'];
+if (!file_exists($invoice)) {
+	echo "File not found :(";
+	die();
+}
+header("Pragma: public");
+header("Expires: 0");
+header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+
+header("Content-Type: application/force-download");
+header( "Content-Disposition: attachment; filename=".basename($invoice));
+
+@readfile($invoice);
+die();
+?>
